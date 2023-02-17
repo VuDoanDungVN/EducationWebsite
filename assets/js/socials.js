8c3c60ce1163cd0f73e65bad6c51f8dc9b09007e
Vu Doan Dung <vudoandungit@gmail.com>
2023-02-17 13:56:55 +0900

    Vu Doan Dung

---
+++ assets/js/socials.js
@@ -0,0 +1,15 @@
+function nolink() {
+    alert("Kick cái zề! Gửi link cho tui thì mới kick được chứ!")
+}
+
+function error() {
+    alert("Xin lỗi ứng dụng đang trong quá trình phát triển!")
+}
+
+function canhbaofirst() {
+    alert("Bạn đang ở trang đầu! Không cần kích thêm đâu!")
+}
+
+function canhbaoend() {
+    alert("Bạn đang ở trang cuối rồi! Không cần kích thêm đâu!")
+}
